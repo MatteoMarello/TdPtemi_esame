899a40c0e52d324e2807ad16831e1a188f7733bf
steeedd <stefanpatru10@yahoo.com>
2024-05-07 14:27:01 +0200

    progress w MeteoParis

--- Classroom/MetroParis/database/DAO.py
+++ Classroom/MetroParis/database/DAO.py
@@ -1,6 +1,6 @@
-from database.DB_connect import DBConnect
-from model.fermata import Fermata
-
+from Classroom.MetroParis.database.DB_connect import DBConnect
+from Classroom.MetroParis.model.fermata import Fermata
+from Classroom.MetroParis.model.connessione import Connessione
 
 class DAO():
 
@@ -16,6 +16,56 @@ class DAO():
 
         for row in cursor:
             result.append(Fermata(row["id_fermata"], row["nome"], row["coordX"], row["coordY"]))
+        cursor.close()
+        conn.close()
+        return result
+
+    @staticmethod
+    def getEdge(v1, v2):
+        conn = DBConnect.get_connection()
+
+        result = []
+
+        cursor = conn.cursor(dictionary=True)
+        query = "SELECT * FROM connessione c WHERE c.id_stazP = %s AND c.id_stazA = %s"
+        cursor.execute(query, (v1.id_fermata, v2.id_fermata))
+
+        for row in cursor:
+            result.append(row)
+        cursor.close()
+        conn.close()
+        return result
+
+
+    @staticmethod
+    def getEdgesVicini(v1):
+        conn = DBConnect.get_connection()
+
+        result = []
+
+        cursor = conn.cursor(dictionary=True)
+        query = "SELECT * FROM connessione c WHERE c.id_stazP = %s"
+        cursor.execute(query, (v1.id_fermata, ))
+
+        for row in cursor:
+            result.append(Connessione(row['id_connessione'], row['id_linea'], row['id_stazP'], row['id_stazA']))
+        cursor.close()
+        conn.close()
+        return result
+
+
+    @staticmethod
+    def getAllConnessioni():
+        conn = DBConnect.get_connection()
+
+        result = []
+
+        cursor = conn.cursor(dictionary=True)
+        query = "SELECT * FROM connessione"
+        cursor.execute(query)
+
+        for row in cursor:
+            result.append(Connessione(row['id_connessione'], row['id_linea'], row['id_stazP'], row['id_stazA']))
         cursor.close()
         conn.close()
         return result
--- Classroom/MetroParis/model/model.py
+++ Classroom/MetroParis/model/model.py
@@ -1,13 +1,68 @@
-from database.DAO import DAO
+from Classroom.MetroParis.database.DAO import DAO
 import networkx as nx
 
 class Model:
     def __init__(self):
         self._fermate = DAO.getAllFermate()
+        self._grafo = nx.DiGraph()
+        self._idMap = {}
+        for f in self._fermate:
+            self._idMap[f.id_fermata] = f
 
     def buildGraph(self):
-        pass
+        self._grafo.add_nodes_from(self._fermate)
+
+        # Modo 1 per aggiungere gli archi: doppio loop sui nodi e query per ogni coppia di nodi per vedere se c'è una linea che collega le due fermate.
+        # Se ci sono tanti nodi come in questo caso, il doppio ciclo sui nodi NON va bene! Quindi bisogna pensare a un altro modo, eventualmente senza
+        # doppio ciclo ma rendendo la query leggermente più complicata...
+        """
+        for u in self._fermate:
+            for v in self._fermate:
+                res = DAO.getEdge(u,v)
+                if len(res) > 0:
+                    self._grafo.add_edge(u, v)
+                    print(f"Added edge between {u} and {v}")
+        """
+
+        # Modo 2 per aggiungere gli archi: rendo la query un po' più complicata, ma ciclo una sola volta sui nodi.
+        # Ciclo sui nodi, mi prendo i vicini di ogni singolo nodo, e aggiungo gli archi tra il nodo e i suoi vicini.
+
+        """
+        for u in self._fermate:
+            vicini = DAO.getEdgesVicini(u)
+            for v in vicini:
+                # -> vicini è una lista di connessioni. Mi devo prendere l'oggetto fermata v_nodo dalla _idMap, in cui
+                # -> ho come chiavi gli id delle fermate e come valori gli oggetti Fermata corrispondenti a quell'id.
+                v_nodo = self._idMap[v.id_stazA]
+                self._grafo.add_edge(u, v_nodo)
+                print(f"Added edge between {u} and {v_nodo}")
+        """
+
+        # Modo 3 per aggiungere gli archi: singola query che mi restituisce direttamente le connessioni e costruisco il grafo
+        # -> senza effettuare alcun loop sulle fermate, ma solo sulle connessioni!
+        # Leggo direttamente tutta la tabella delle connessioni e la gestisco nel model aggiungendo i diversi archi tra i nodi: lo posso fare perchè ho una lista con tutte le connessioni!
+
+        all_connessioni = DAO.getAllConnessioni()
+        for c in all_connessioni:
+            u_nodo = self._idMap[c.id_stazP]
+            v_nodo = self._idMap[c.id_stazA]
+            self._grafo.add_edge(u_nodo, v_nodo)
+            # print(f"Added edge between {u_nodo} and {v_nodo}")
+
 
     @property
     def fermate(self):
         return self._fermate
+
+    def getNumNodes(self):
+        return len(self._grafo.nodes)
+
+    def getNumEdges(self):
+        return len(self._grafo.edges)
+
+
+if __name__ == "__main__":
+    mymodel = Model()
+    mymodel.buildGraph()
+    print(f'The graph has {mymodel.getNumNodes()} nodes.')
+    print(f'The graph has {mymodel.getNumEdges()} edges.')
