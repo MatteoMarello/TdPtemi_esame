e305294b5bdbac90bfd3054af31415bff7a48395
steeedd <stefanpatru10@yahoo.com>
2024-05-07 15:54:46 +0200

    added PolizzAssicurativa

--- Lab/Lab9/model/model.py
+++ Lab/Lab9/model/model.py
@@ -1,3 +1,19 @@
+from Lab.Lab9.database.DAO import DAO
+import networkx as nx
 class Model:
     def __init__(self):
-        pass
+        self._airports = DAO.getAllAirports()
+        self._airportsMap = {}
+        for a in self._airports:
+            self._airportsMap[a.id] = a
+
+
+    def buildGraph(self):
+        self._graphAirports = nx.Graph()
+        for airport in self._airportsMap.values():
+            self._graphAirports.add_node(airport)
+
+
+if __name__ == "__main__":
+    model = Model()
+    model.buildGraph()
